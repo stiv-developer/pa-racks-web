ff910dd58bb37d23587ee49384a5ad13c97fd200
stiv-dev-front <srua@gato.pe>
2026-02-16 13:58:01 -0500

    Deploy setup

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,4 +2,9 @@
 import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
-export default defineConfig({});
+
+export default defineConfig({
+    site: 'https://stiv-developer.github.io',
+    base: '/pa-racks-web/',
+    output: 'static'
+})

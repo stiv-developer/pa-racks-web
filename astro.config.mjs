8ca4d1e1c708e2558968c4acd877f29a5ebb0eb5
stiv-dev-front <srua@gato.pe>
2026-02-19 08:57:12 -0500

    rename site

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,7 +7,6 @@ import sitemap from '@astrojs/sitemap'
 export default defineConfig({
     // site: 'https://stiv-developer.github.io',
     site: 'pa-racks-oil1edasp-stivs-projects-07e042c8.vercel.app',
-    base: '/pa-racks-web/',
     output: 'static',
     integrations: [sitemap()]
 })

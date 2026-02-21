903352ce99d1500a975568afed22e7ca4d82e310
stiv-dev-front <srua@gato.pe>
2026-02-21 10:29:43 -0500

    add images card

---
+++ src/data/projects.js
@@ -0,0 +1,74 @@
+export const projects = [
+{
+    title: "RACK SELECTIVO",
+    slug: "rack-selectivo",
+    category: "pesada",
+    description:
+      "Solución que incorpora un sistema de rodillos con una ligera inclinación, lo que le permite el deslizamiento de las pa..",
+    image: "images/sistema-almacenamiento/rack-selectivo.jpeg",
+  },
+  {
+    title: "RACK ACUMULATIVO",
+    slug: "rack-acumulativo",
+    category: "pesada",
+    description:
+      "Solución que incorpora un sistema de rodillos con una ligera inclinación, lo que le permite el deslizamiento de las pa..",
+    image: "images/sistema-almacenamiento/rack-acumulativo.jpeg",
+  },
+  {
+    title: "CANTILEVER",
+    slug: "cantilever",
+    category: "pesada",
+    description:
+      "Solución que incorpora un sistema de rodillos con una ligera inclinación, lo que le permite el deslizamiento de las pa..",
+    image: "images/sistema-almacenamiento/cantilever.jpeg",
+  },
+  {
+    title: "RACK AUTOPORTANTE",
+    slug: "rack-autoportante",
+    category: "pesada",
+    description:
+      "Solución que incorpora un sistema de rodillos con una ligera inclinación, lo que le permite el deslizamiento de las pa..",
+    image: "images/sistema-almacenamiento/autoportante.jpeg",
+  },
+  {
+    title: "MINI RACK",
+    slug: "mini-rack",
+    category: "semi",
+    description:
+      "Solución que incorpora un sistema de rodillos con una ligera inclinación, lo que le permite el deslizamiento de las pa..",
+    image: "images/sistema-almacenamiento/mini-racks.jpeg",
+  },
+  {
+    title: "ENTREPISO RACK / ESTANTERIA",
+    slug: "entrepiso-rack-estanteria",
+    category: "semi",
+    description:
+      "Solución que incorpora un sistema de rodillos con una ligera inclinación, lo que le permite el deslizamiento de las pa..",
+    image: "images/sistema-almacenamiento/rack-entrepiso.jpeg",
+  },
+  {
+    title: "ESTANTERÍA METALICA FIJA",
+    slug: "estanteria-metalica-fija",
+    category: "ligera",
+    description:
+      "Solución que incorpora un sistema de rodillos con una ligera inclinación, lo que le permite el deslizamiento de las pa..",
+    image: "images/sistema-almacenamiento/estanteria-fija.jpeg",
+  },
+  {
+    title: "ESTANTERÍA METALICA MOVIL",
+    slug: "estanteria-metalica-movil",
+    category: "ligera",
+    description:
+      "Solución que incorpora un sistema de rodillos con una ligera inclinación, lo que le permite el deslizamiento de las pa..",
+    image: "images/sistema-almacenamiento/estanteria-movil.jpeg",
+  },
+  {
+    title: "MEZZANINE ESTRUCTURAL",
+    slug: "mezzanine-estructural",
+    category: "especial",
+    description:
+      "Solución que incorpora un sistema de rodillos con una ligera inclinación, lo que le permite el deslizamiento de las pa..",
+    image: "images/sistema-almacenamiento/mezanine.jpeg",
+  },
+];
